7b3f8156fc75b872c3956b566e39d758c88e7d6b
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2020-08-18 11:33:58 +0530

    Add files via upload

---
+++ Array/08 Improving Linear search.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+struct Array {
+	int A[20];
+	int length;
+	int size;
+	
+};
+void Display (struct Array arr)
+{
+	int i;
+	printf("\nEnter elements\n");
+	for (i=0;i<arr.length;i++)
+	printf("%d ",arr.A[i]);
+}
+void swap(int *x,int *y)
+ {
+ int temp=*x;
+ *x=*y;
+ *y=temp;
+ }
+int LinearSearch(struct Array *arr,int key)
+{
+ int i;
+ for(i=0;i<arr->length;i++)
+ {
+ if(key==arr->A[i])
+ {
+ swap(&arr->A[i],&arr->A[0]);
+ return i;
+ }
+ }
+ return -1;
+}
+
+int main ()
+{
+	struct Array arr1= {{2,3,4,2,3,4,5,88,9	},20,5	};
+	printf("The element is at %dth index",LinearSearch(&arr1,9));
+	Display(arr1);
+	return 0;
+}
