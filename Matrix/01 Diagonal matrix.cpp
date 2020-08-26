e403bc76a92363b17e5a9fe18072f95b0fd04be1
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2020-08-26 20:56:30 +0530

    Add files via upload

--- Matrix/01 Diagonal matrix.cpp
+++ Matrix/01 Diagonal matrix.cpp
@@ -1,75 +1,67 @@
-// Diagonal Matrix CPP
 #include <iostream>
 using namespace std;
-class Diagonal
+class Diagonal // Difining a diagonal class 
 {
-private:
- int *A;
- int n;
-public:
- Diagonal()
- {
- n=2;
- A=new int[2];
- }
- Diagonal(int n)
- {
- this->n=n;
- A=new int[n];
- }
- ~Diagonal()
- {
- delete []A;
- }
- void Set(int i,int j,int x);
- int Get(int i,int j);
- void Display();
- int GetDimension(){return n;}
+	private: // passing datamembers with private
+		int n; // Dimension
+		int *A; // array as pointer
+	public: // inside public the very 1st thing is constructor 
+	Diagonal() // non parametrized constructor 
+	{
+		n=2;
+		A= new int[2];
+	}
+	Diagonal (int n) // parametarized constructor 
+	{
+		this->n=n;
+		A=new int [n];
+	}
+	~Diagonal () // since we are creating memory in heap so for realissing memory we will use destructor 
+	{
+		delete []A;
+	}
+	void Set (int i, int j, int x);
+	int Get (int i, int j);
+	void Display();
 };
 void Diagonal::Set(int i,int j,int x)
 {
- if(i==j)
- A[i-1]=x;
+	if (i==j)
+	A[i-1]=x;
 }
 int Diagonal::Get(int i,int j)
 {
- if(i==j)
- return A[i-1];
- return 0;
+	if (i==j)
+	{
+		return A[i-1];
+		
+	}
+	else return 0;
 }
 void Diagonal::Display()
+
 {
- for(int i=1;i<=n;i++)
- {
- for(int j=1;j<=n;j++)
- {
- if(i==j)
- cout<<A[i-1]<<" ";
- else
- cout<<"0 ";
- }
- cout<<endl;
- }
+for (int i=0;i<n;i++)
+{
+	for (int j=0;j<n;j++)
+	{
+		if (i==j)
+		cout << A[i]<<" ";
+		else 
+		cout<<"0 ";
+	}
+	cout<<endl;
 }
-int main()
+}
+
+int main ()
+
 {
- int d;
- cout<<"Enter Dimensions";
- cin>>d;
-
- Diagonal dm(d);
-
- int x;
- cout<<"Enter All Elements";
- for(int i=1;i<=d;i++)
- {
- for(int j=1;j<=d;j++)
- {
- cin>>x;
- dm.Set(i,j,x);
- }
- }
-
- dm.Display();
- return 0;
+	Diagonal d(4);
+	d.Set(1,1,5);
+	d.Set(2,2,9);
+	d.Set(3,3,7);
+	d.Set(4,4,2);
+	d.Display();
+	return 0;
 }
