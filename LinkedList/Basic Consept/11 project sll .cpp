19e1e78688ccc75b5cfed180c58ff25ef6c29515
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2020-09-03 23:46:48 +0530

    Update 11 project sll .cpp

--- LinkedList/Basic Consept/11 project sll .cpp
+++ LinkedList/Basic Consept/11 project sll .cpp
@@ -19,7 +19,7 @@ void Create(int A[], int n)
 	first->next = NULL;
 	last = first;
 
-	for (i = 0; i < n; i++)
+	for (i = 1; i < n; i++)
 	{
 		t = (struct Node*)malloc(sizeof(struct Node));
 		t->data = A[i];
