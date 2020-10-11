b0420a3185828ea26307e0a28088eca49b69d12b
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2020-10-11 21:11:10 +0530

    Update 02 head recursion.cpp

--- Recursion/02 head recursion.cpp
+++ Recursion/02 head recursion.cpp
@@ -1,14 +1,14 @@
 #include<stdio.h>
 void func1(int n)
 {
-	if (n>0)
+	if (n>0) // Base condition
 	{
-		func1(n-1);
+		func1(n-1); // 1st call then print 
 		printf("%d ",n);
 	}
 }
 int main (){
 	int x=9;
-	func1(x);
+	func1(x); // passing parameter to fxn 
 	return 0;
 }
