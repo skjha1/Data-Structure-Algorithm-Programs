b8d0c848e9f17b15a72be257d3682dab04642d37
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2020-10-24 21:38:42 +0530

    Add files via upload

---
+++ src/cpp/02 Constants, Manipulators & Operator Precedence.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
+int main(){
+    // int a = 34; 
+    // cout<<"The value of a was: "<<a;
+    // a = 45; 
+    // cout<<"The value of a is: "<<a;
+    // Constants in C++
+    // const int a = 3;
+    // cout<<"The value of a was: "<<a<<endl;
+    // a = 45; // You will get an error because a is a constant
+    // cout<<"The value of a is: "<<a<<endl;
+
+    // Manipulators in C++
+    // int a =3, b=78, c=1233;
+    // cout<<"The value of a without setw is: "<<a<<endl;
+    // cout<<"The value of b without setw is: "<<b<<endl;
+    // cout<<"The value of c without setw is: "<<c<<endl;
+
+    // cout<<"The value of a is: "<<setw(4)<<a<<endl;
+    // cout<<"The value of b is: "<<setw(4)<<b<<endl;
+    // cout<<"The value of c is: "<<setw(4)<<c<<endl;
+
+
+    // Operator Precedence
+    int a =3, b=4;
+    // int c = (a*5)+b;
+    int c = ((((a*5)+b)-45)+87);
+    cout<<c;
+    return 0;
+//https://codewithharry.com/videos/cpp-tutorials-in-hindi-8
+//https://en.cppreference.com/w/cpp/language/operator_precedence
+}
---
+++ src/cpp/03 If Else and Switch-Case Statement.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    // cout<<"This is tutorial 9";
+    int age;
+    cout<< "Tell me your age"<<endl;
+    cin>>age;
+
+    // 1. Selection control structure: If else-if else ladder
+    // if((age<18) && (age>0)){
+    //     cout<<"You can not come to my party"<<endl;
+    // }
+    // else if(age==18){
+    //     cout<<"You are a kid and you will get a kid pass to the party"<<endl;
+    // }
+    // else if(age<1){
+    //     cout<<"You are not yet born"<<endl;
+    // }
+    // else{
+    //     cout<<"You can come to the party"<<endl;
+    // }
+
+    // 2. Selection control structure: Switch Case statements
+    switch (age)
+    {
+    case 18: 
+        cout<<"You are 18"<<endl;
+        break;
+    case 22: 
+        cout<<"You are 22"<<endl;
+        break;
+    case 2: 
+        cout<<"You are 2"<<endl;
+        break;
+
+    default:
+    cout<<"No special cases"<<endl;
+        break;
+    }
+
+    cout<<"Done with switch case";
+    return 0;
+}
+
+/* Control Structures in C++
+The work of control structures is to give flow and logic to a program. There are three types of basic control structures in C++.
+
+Sequence Structure
+Sequence structure refers to the sequence in which program execute instructions one after another.  
+
+Selection Structure
+Selection structure refers to the execution of instruction according to the selected 
+condition, which can be either true or false. There are two ways to implement 
+selection structures, by “if-else statements” or by “switch case statements”.
+
+Loop Structure
+Loop structure refers to the execution of an instruction in a loop until the condition gets false.
+
+*/
