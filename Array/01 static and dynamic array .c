f0942d202a83d8f9ed19887bf00aafeca1401af0
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2020-10-28 15:21:36 +0530

    Update 01 static and dynamic array .c

--- Array/01 static and dynamic array .c
+++ Array/01 static and dynamic array .c
@@ -1,28 +1,25 @@
-#include<stdio.h>
-#include<stdlib.h>
-int main ()
+#include <stdio.h>
+#include <stdlib.h>
+int main()
 {
-	int A[5]={2,3,4,6,7};  // this will allocate memory in stack memory 
-	int *p;
+	int A[5] = { 1,2,3,4,5 }; // this is allocated inside stack memory 
+	int* p; // pointer of array
 	int i;
-	
-	p= (int *)malloc(5*sizeof(int)); // using malloc we can access memory in heap 
-	p[0]=8;
-	p[1]=7;
-	p[2]=9;
-	p[3]=10;
-	p[4]=12;
-	
-	for (i=0;i<5;i++)
+	p = (int*)malloc(5 * sizeof(int)); // dynamically createing array inside heap 
+	p[0] = 1; // initializing all the terms in array
+	p[1] = 2;
+	p[2] = 3;
+	p[3] = 4;
+	p[4] = 5;
+
+	for (i = 0; i < 5; i++) // iteration for normal array 
 	{
-		printf("%d ",A[i]);
-		
+		printf("%d ", A[i]);
 	}
 	printf("\n");
-	for (i=0;i<5;i++)
+ 	for (i = 0; i < 5; i++) // iteration for the dynamic array 
 	{
 		printf("%d ",p[i]);
-		
 	}
 	return 0;
 }
