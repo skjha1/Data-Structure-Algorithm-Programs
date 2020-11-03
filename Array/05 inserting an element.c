020b798dff44d07888f1ab83704eb306325ab3b6
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2020-11-03 13:52:07 +0530

    Update 05 inserting an element.c

--- Array/05 inserting an element.c
+++ Array/05 inserting an element.c
@@ -1,39 +1,42 @@
-#include<stdio.h>
+#include <iostream>
+using namespace std;
 struct Array
 {
- int A[10];
- int size;
- int length;
+	int A[10];
+	int size;
+	int length;
 };
- void Display(struct Array arr)
- {
- int i;
- printf("\nElements are\n");
- for(i=0;i<arr.length;i++)
- printf("%d ",arr.A[i]);
- }
- void Append(struct Array *arr,int x)
- {
- if(arr->length<arr->size)
- arr->A[arr->length++]=x;
- }
- void Insert(struct Array *arr,int index,int x)
- {
- int i;
+void Display(struct Array arr)
+{
+	int i;
+	for (i = 0; i < arr.length; i++)
+		cout << arr.A[i] << " ";
+}
+void Add(struct Array *arr , int x) // element to be inserted 
+{
+	if (arr->length < arr->size)
+		arr->A[arr->length++] = x; // increment the size of array and fit the elements // it will take the constant time 
+}
+void Insert(struct Array* arr, int index, int x) // the index at which we want to enter // and the value we want tpo enter 
+{
+	int i;
+	for (i = arr->length; i > index; i--) // we will strat from last of the array and shift the elements upto the given index 
+	{
+		arr->A[i] = arr->A[i - 1]; // shifting the elements to next  // i is on some index and copy the value from previous index 
+	}
+	arr->A[index] = x; // set the value at the particular index 
+	arr->length++; // increment the size of length 
+	// work done shifting of elelmets and copying elements 
+	// best case : --------------- min : O(1) // there may be zero shifting 
+	// worst case : --------------- max : O(n)// there may be n shifing 
 
- if(index>=0 && index <=arr->length)
- {
- for(i=arr->length;i>index;i--)
- arr->A[i]=arr->A[i-1];
- arr->A[index]=x;
- arr->length++;
- }
- }
+}
+
 int main()
 {
-struct Array arr1={{2,3,4,5,6},10,5};
-Append(&arr1,10);
-Insert(&arr1,0,12);
-Display(arr1);
- return 0;
+	struct Array arr1 = { {2,3,4,5,6},10,5 };
+	Add(&arr1, 10);
+	Insert(&arr1, 0, 12);
+	Display(arr1);
+	return 0;
 }
