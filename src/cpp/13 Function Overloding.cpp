f9857b0f55c2cd67cb1b1e183b00b2d95c443df5
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2020-11-06 13:15:08 +0530

    Add files via upload

---
+++ src/cpp/13 Function Overloding.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+int sum(float a, int b){
+    cout<<"Using function with 2 arguments"<<endl;
+    return a+b;
+}
+
+int sum(int a, int b, int c){
+    cout<<"Using function with 3 arguments"<<endl;
+    return a+b+c;
+}
+
+// Calculate the volume of a cylinder
+int volume(double r, int h){
+    return(3.14 * r *r *h);
+}
+
+// Calculate the volume of a cube
+int volume(int a){
+    return (a * a * a);
+}
+
+// Rectangular box
+int volume (int l, int b, int h){
+    return (l*b*h);
+}
+
+int main(){
+    cout<<"The sum of 3 and 6 is "<<sum(3,6)<<endl;
+    cout<<"The sum of 3, 7 and 6 is "<<sum(3, 7, 6)<<endl;
+    cout<<"The volume of cuboid of 3, 7 and 6 is "<<volume(3, 7, 6)<<endl;
+    cout<<"The volume of cylinder of radius 3 and height 6 is "<<volume(3, 6)<<endl;
+    cout<<"The volume of cube of side 3 is "<<volume(3)<<endl;
+    return 0;
+}
+/* 
+
+Function overloading is a process to make more than one function
+ with the same name but different parameters, numbers, or sequence.
+https://codewithharry.com/videos/cpp-tutorials-in-hindi-19
+*/
