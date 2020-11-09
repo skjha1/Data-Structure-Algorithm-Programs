1a4e0065a22ce6516f380028bf56495173e95e1a
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2020-11-09 12:08:49 +0530

    Create 18 Array of Objects & Passing Objects as Function Arguments.cpp

---
+++ src/cpp/18 Array of Objects & Passing Objects as Function Arguments.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+class complex{
+    int a;
+    int b;
+
+    public: 
+        void setData(int v1, int v2){
+            a = v1;
+            b = v2;
+        }
+
+        void setDataBySum(complex o1, complex o2){
+            a = o1.a + o2.a;
+            b = o1.b + o2.b;
+        }
+
+        void printNumber(){
+            cout<<"Your complex number is "<<a<<" + "<<b<<"i"<<endl;
+        }
+};
+
+int main(){
+    complex c1, c2, c3;
+    c1.setData(1, 2);
+    c1.printNumber();
+
+    c2.setData(3, 4);
+    c2.printNumber();
+
+    c3.setDataBySum(c1, c2);
+    c3.printNumber();
+    return 0;
+}
+
+
+/* 
+An array of objects is declared the same as any other data-type array.  An array of objects consists of class objects as its elements. 
+If the array consists of class objects it is called an array of objects.
+
+Passing Object as Function Argument
+Objects can be passed as function arguments. This is useful when we want to assign the values of a passed object to the current object. 
+*/
