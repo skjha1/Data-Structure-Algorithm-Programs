02b94ec6ce6522fdde29990cd6aea69e24fb0192
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2020-11-09 10:40:11 +0530

    Create 16 Objects Memory Allocation & using Arrays in Classes.cpp

---
+++ src/cpp/16 Objects Memory Allocation & using Arrays in Classes.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class Shop
+{
+    int itemId[100];
+    int itemPrice[100];
+    int counter;
+
+public:
+    void initCounter(void) { counter = 0; }
+    void setPrice(void);
+    void displayPrice(void);
+};
+
+void Shop ::setPrice(void)
+{
+    cout << "Enter Id of your item no " << counter + 1 << endl;
+    cin >> itemId[counter];
+    cout << "Enter Price of your item" << endl;
+    cin >> itemPrice[counter];
+    counter++;
+}
+
+void Shop ::displayPrice(void)
+{
+    for (int i = 0; i < counter; i++)
+    {
+        cout << "The Price of item with Id " << itemId[i] << " is " << itemPrice[i] << endl;
+    }
+}
+
+int main()
+{
+    Shop dukaan;
+    dukaan.initCounter();
+    dukaan.setPrice();
+    dukaan.setPrice();
+    dukaan.setPrice();
+    dukaan.displayPrice();
+    return 0;
+}
