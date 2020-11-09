a88c613fbbcb1fec1d4871399f9cdcac3d76f688
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2020-11-09 12:52:56 +0530

    Create 20 Friend Classes & Member Friend Functions.cpp

---
+++ src/cpp/20 Friend Classes & Member Friend Functions.cpp
@@ -0,0 +1,73 @@
+/* 
+Member Friend Functions in C++
+Friend functions are those function which has access to private members of the class in which they are declared. The main thing to note here is that only that function 
+can access the member function which is made friend of the other class. An example of a friend function is shown below.
+
+Friend Classes in C++
+Friend classes are those classes which have the permission to access private members of the class in which they are declared. The main thing to note here is that if the
+class is made friend of another class then it can access all the private members of that class. 
+*/
+
+#include <iostream>
+using namespace std;
+
+// Forward declaration
+class Complex;
+
+class Calculator
+{
+public:
+    int add(int a, int b)
+    {
+        return (a + b);
+    }
+
+    int sumRealComplex(Complex, Complex);
+    int sumCompComplex(Complex, Complex);
+};
+
+class Complex
+{
+    int a, b;
+    // Individually declaring functions as friends
+    // friend int Calculator ::sumRealComplex(Complex, Complex);
+    // friend int Calculator ::sumCompComplex(Complex, Complex);
+
+    // Aliter: Declaring the entire calculator class as friend
+    friend class Calculator;
+
+public:
+    void setNumber(int n1, int n2)
+    {
+        a = n1;
+        b = n2;
+    }
+
+    void printNumber()
+    {
+        cout << "Your number is " << a << " + " << b << "i" << endl;
+    }
+};
+
+int Calculator ::sumRealComplex(Complex o1, Complex o2)
+{
+    return (o1.a + o2.a);
+}
+
+int Calculator ::sumCompComplex(Complex o1, Complex o2)
+{
+    return (o1.b + o2.b);
+}
+
+int main()
+{
+    Complex o1, o2;
+    o1.setNumber(1, 4);
+    o2.setNumber(5, 7);
+    Calculator calc;
+    int res = calc.sumRealComplex(o1, o2);
+    cout << "The sum of real part of o1 and o2 is " << res << endl;
+    int resc = calc.sumCompComplex(o1, o2);
+    cout << "The sum of complex part of o1 and o2 is " << resc << endl;
+    return 0;
+}
