eff38ae43ca51c7c4410d147364fe0058f385747
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2020-11-09 11:42:11 +0530

    Create 17 Static Data Members & Methods.cpp

---
+++ src/cpp/17 Static Data Members & Methods.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+class Employee
+{
+    int id;
+    static int count;
+
+public:
+    void setData(void)
+    {
+        cout << "Enter the id" << endl;
+        cin >> id;
+        count++;
+    }
+    void getData(void)
+    {
+        cout << "The id of this employee is " << id << " and this is employee number " << count << endl;
+    }
+
+    static void getCount(void){
+        // cout<<id; // throws an error
+        cout<<"The value of count is "<<count<<endl;
+    }
+};
+
+// Count is the static data member of class Employee
+int Employee::count; // Default value is 0
+
+int main()
+{
+    Employee harry, rohan, lovish;
+    // harry.id = 1;
+    // harry.count=1; // cannot do this as id and count are private
+
+    harry.setData();
+    harry.getData();
+    Employee::getCount();
+
+    rohan.setData();
+    rohan.getData();
+    Employee::getCount();
+
+    lovish.setData();
+    lovish.getData();
+    Employee::getCount();
+
+    return 0;
+}
+/* When a static data member is created, there is only a single copy of the data member which is shared between all the objects of the class. 
+As we have discussed in our previous lecture that if the data members are not static then every object has an individual copy of the data member and it is not shared.
+
+
+When a static method is created, they become independent of any object and class. Static methods can only access static data members and static methods.
+Static methods can only be accessed using the scope resolution operator.
+*/
