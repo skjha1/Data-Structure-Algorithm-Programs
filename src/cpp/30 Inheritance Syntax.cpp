914b067d7d1372f2a2b2f62069e79560f9cebc6c
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2020-11-11 18:56:44 +0530

    Create 30 Inheritance Syntax.cpp

---
+++ src/cpp/30 Inheritance Syntax.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+// Base Class
+class Employee
+{
+public:
+    int id;
+    float salary;
+    Employee(int inpId)
+    {
+        id = inpId;
+        salary = 34.0;
+    }
+    Employee() {}
+};
+
+// Derived Class syntax
+/*
+class {{derived-class-name}} : {{visibility-mode}} {{base-class-name}}
+{
+    class members/methods/etc...
+}
+Note:
+1. Default visibility mode is private
+2. Public Visibility Mode: Public members of the base class becomes Public members of the derived class
+3. Private Visibility Mode: Public members of the base class becomes Private members of the derived class
+4. Private members are never inherited
+*/
+
+// Creating a Programmer class derived from Employee Base class
+class Programmer : public Employee
+{
+public:
+    int languageCode;
+    Programmer(int inpId)
+    {
+        id = inpId;
+        languageCode = 9;
+    }
+    void getData(){
+        cout<<id<<endl;
+    }
+};
+
+int main()
+{
+    Employee harry(1), rohan(2);
+    cout << harry.salary << endl;
+    cout << rohan.salary << endl;
+    Programmer skillF(10);
+    cout << skillF.languageCode<<endl;
+    cout << skillF.id<<endl;
+    skillF.getData();
+    return 0;
+}
