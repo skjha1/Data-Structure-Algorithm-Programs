8892895f0f9bbcac9a4be220252afefad471d939
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2020-11-13 21:42:42 +0530

    Create 33 Multilevel Inheritance.cpp

---
+++ src/cpp/33 Multilevel Inheritance.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+
+class Student
+{
+protected:
+    int roll_number;
+
+public:
+    void set_roll_number(int);
+    void get_roll_number(void);
+};
+
+void Student ::set_roll_number(int r)
+{
+    roll_number = r;
+}
+
+void Student ::get_roll_number()
+{
+    cout << "The roll number is " << roll_number << endl;
+}
+
+class Exam : public Student
+{
+protected:
+    float maths;
+    float physics;
+
+public:
+    void set_marks(float, float);
+    void get_marks(void);
+};
+
+void Exam ::set_marks(float m1, float m2)
+{
+    maths = m1;
+    physics = m2;
+}
+
+void Exam ::get_marks()
+{
+    cout << "The marks obtained in maths are: " << maths << endl;
+    cout << "The marks obtained in physics are: " << physics << endl;
+}
+
+class Result : public Exam
+{
+    float percentage;
+
+public:
+    void display_results()
+    {
+        get_roll_number();
+        get_marks();
+        cout << "Your result is " << (maths + physics) / 2 << "%" << endl;
+    }
+};
+
+int main()
+{
+    /*
+    Notes: 
+        If we are inheriting B from A and C from B:[ A--->B--->C ]
+        1. A is the base class for B and B is the base class for C
+        2. A-->B-->C is called Inheritance Path
+    */
+
+    Result harry;
+    harry.set_roll_number(420);
+    harry.set_marks(94.0, 90.0);
+    harry.display_results();
+    return 0;
+}
