a16acd40b2a7c297bbd9b7bef08ec88d4adb899d
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2020-11-13 21:41:38 +0530

    Create 32 Protected Access Modifier.cpp

---
+++ src/cpp/32 Protected Access Modifier.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+class Base{
+    protected:
+        int a; 
+    private:
+        int b;
+
+};
+/*
+For a protected member:
+                        Public derivation   Private Derivation   Protected Derivation
+    1. Private members      Not Inherited   Not Inherited       Not Inherited
+    2. Protected members    Protected       Private             Protected
+    3. Public members       Public          Private             Protected
+*/
+class Derived: protected Base{
+   
+};
+
+int main(){
+    Base b;
+    Derived d;
+    // cout<<d.a; // Will not work since a is protected in both base as well as derived class
+    return 0;
+}
