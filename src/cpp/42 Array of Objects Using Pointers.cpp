b3774156d9305be7ede32c0696fe47149bc3383b
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2020-11-14 11:38:41 +0530

    Create 42 Array of Objects Using Pointers.cpp

---
+++ src/cpp/42 Array of Objects Using Pointers.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+
+class ShopItem
+{
+    int id;
+    float price;
+    public:
+        void setData(int a, float b){
+            id = a;
+            price = b;
+        }
+        void getData(void){
+            cout<<"Code of this item is "<< id<<endl;
+            cout<<"Price of this item is "<<price<<endl;
+        }
+};
+        // 1 2 3
+        //     ^
+        //     |
+        //     |
+        //     ptr
+        // ptrTemp
+int main(){
+    int size = 3;
+    // int *ptr = &size;
+    // int *ptr = new int [34];
+
+    // 1. general store item
+    // 2. veggies item
+    // 3. hardware item
+    ShopItem *ptr = new ShopItem [size];
+    ShopItem *ptrTemp = ptr;
+    int p, i;
+    float q;
+    for (i = 0; i < size; i++)
+    {
+        cout<<"Enter Id and price of item "<< i+1<<endl;
+        cin>>p>>q;
+        // (*ptr).setData(p, q);
+        ptr->setData(p, q);
+        ptr++; 
+    }
+
+    for (i = 0; i < size; i++)
+    {
+        cout<<"Item number: "<<i+1<<endl;
+        ptrTemp->getData();
+        ptrTemp++;
+    }
+    
+    
+    return 0;
+}
