96df9cb40110214e518bb310269993c0b5be7c86
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2020-11-23 06:39:30 +0530

    Update 06 Deleting element from array.c

--- Array/06 Deleting element from array.c
+++ Array/06 Deleting element from array.c
@@ -26,10 +26,12 @@ The Arrow(->) operator exists to access the members of the structure or the unio
 	int i;
 	if (index>=0 && index <arr->length)
 	{
-		x=arr->A[index]; // first of all take out the element from the particular index you want to del
+		x=arr->A[index]; // first of all take out the element from the particular index you want to del---------------------> taking 1 unit of time 
 		for (i=index;i<arr->length-1;i++) // shift the element in forward direnction and stop at length -1 so that array can not have any vacant space 
 		arr->A[i]=arr->A[i+1]; // at the place of a[i] copy the next element i.e a[i+1]
-		arr->length--;//at last the length will be decreatmented 
+		arr->length--;//at last the length will be decreatmented ----------------> 1 unit of time -------> so best time is 1+1 =2 
+		
+		// min shifting is 0 and max shifting is n ----> Best case time is O(1) -> Worst Case time is ----> O(n)
 		return x;
 	}
 	return 0;
