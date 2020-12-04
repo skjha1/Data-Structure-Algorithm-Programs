60af5336052f3c886ef3036b11a983571ca6e03c
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2020-12-04 19:24:35 +0530

    Update 12 Rotate and Shift.c

--- Array/12 Rotate and Shift.c
+++ Array/12 Rotate and Shift.c
@@ -13,32 +13,31 @@ void Display (struct Array arr)
 	printf("%d ",arr.A[i]);
 	
 }
-void swap(int *x,int *y)
+void Swap(int *x,int *y)
 {
 	int temp=*x;
 	*x=*y;
 	*y=temp;
 }
 
-void Reverse(struct Array* arr)
+void Reverse(struct Array* arr) // function for reversing the array 
 {
-	int* B;
+	int* B; // making a pointer typr array 
 	int i, j;
-	B = new int[arr->size];
-	for (i = arr->length-1, j = 0; i >= 0; i--, j++)
-		B[j] = arr->A[i];
+	B = new int[arr->size]; // creating the required size of array in heap 
+	for (i = arr->length - 1, j = 0; i >= 0; i--, j++) // keeping all the elements from array a to b in reverse order 
+		B[j] = arr->A[i]; // coping element everytime 
 	for (i = 0; i < arr->length; i++)
-		arr->A[i] = B[i];
-}
-void Reverse1(struct Array *arr)
-{
-	int i,j;
-	for (i=0,j=arr->length-1;i<j;i++,j--)
-	{
-		swap(&arr->A[i],&arr->A[j]);
-	}
+		arr->A[i] = B[i]; // copying back elements to A 
 }
 
+void Reverse1(struct Array* arr)
+{
+	int i, j;
+	for (i = 0, j = arr->length - 1; i < j; i++, j--) // we will interchange 1st with the last element
+		Swap(&arr->A[i], &arr->A[j]); // swapping 1st with the last 
+
+}
 
 
 int main()
