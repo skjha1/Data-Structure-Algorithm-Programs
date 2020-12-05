0f89f5f850a6a156dcbf9f37d95b53b5d8c915be
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2020-12-05 20:23:52 +0530

    Update 13 IsSorted .c

--- Array/13 IsSorted .c
+++ Array/13 IsSorted .c
@@ -1,33 +1,44 @@
-//Checking if Array is Sorted
-#include<stdio.h>
-#include<stdlib.h>
+#include<iostream>
+using namespace std;
+
 struct Array
 {
- int A[10];
- int size;
- int length;
+	int* A;
+	int size;
+	int length;
 };
- void Display(struct Array arr)
- {
- int i;
- printf("\nElements are\n");
- for(i=0;i<arr.length;i++)
- printf("%d ",arr.A[i]);
- }
-int isSorted(struct Array arr)
+void Display(struct Array* arr)
 {
- int i;
- for(i=0;i<arr.length-1;i++)
- {
- if(arr.A[i]>arr.A[i+1])
- return 0;
- }
- return 1;
+	int i;
+	cout << "the elements of the array is " << endl;
+	for (i = 0; i < arr->length; i++)
+		cout <<arr->A[i] << " ";
+}
+
+int IsSorted(struct Array* arr)
+{
+	for (int i = 0; i < arr->length - 1; i++) 
+	{
+		if (arr->A[i] > arr->A[i + 1]) // if 1st element is greater then 2nd element then it will k/as not sorted so return 0 
+			return 0;
+	}
+	
+	return 1; // if everything is ok and the element came successfully out of loop means it is sorted return 1 for thet 
 }
 int main()
 {
- struct Array arr1={{2,3,9,16,18,21,28,32,35},10,9};
- printf("%d",isSorted(arr1));
- Display(arr1);
+	struct Array arr;
+	cout << "Enter the size of Array " << endl;
+	cin >> arr.size;
+	arr.A = new int[arr.size];	
+	int no;
+	cout << "Enter the length of the array " << endl;
+	cin >> no;
+	arr.length = 0;	
+	cout << "Enter the elements of the array " << endl;
+	for (int i = 0; i < no; i++)
+		cin >> arr.A[i];
+	arr.length = no;
+	cout<<IsSorted(&arr);
  return 0;
 }
