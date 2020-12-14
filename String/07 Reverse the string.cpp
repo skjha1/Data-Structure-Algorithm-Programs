243be190fcd1579c993da7d123100dd4ae85a5c6
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2020-12-14 12:04:50 +0530

    Update 07 Reverse the string.cpp

--- String/07 Reverse the string.cpp
+++ String/07 Reverse the string.cpp
@@ -1,19 +1,50 @@
 #include<iostream>
 using namespace std;
-int main ()
+void Reverse(char s[], int no)
 {
-	char A[]="python";
-	char B[7];
-	int i;
-	for (i=0;A[i]!='\0';i++)
+	char* B; // string in heap 
+	B = new char[no];
+	int i, j;
+	for (i = 0; s[i] != '\0'; i++) // taking i upto last 
 	{
-	
+
 	}
-	i=i-1;
-	for (int j=0;i>=0;i--,j++)
+	i = i - 1; // setting i one last of string 
+	for (j = 0; i >= 0; i--, j++) // incrementing j and decrementing i upto i is greater then 0 
+		B[j] = s[i]; 
+	B[j] = '\0';
+	cout << "The reverse of string is " << B;
+}
+void Reverse1(char s[])
+{
+	int i, temp, j;
+	for (j = 0; s[j] != '\0'; j++) // taking j to last of the string 
 	{
-		B[j]=A[i];
 	}
-	B[i]='\0';
-	cout<<"Reverse of the string is "<<endl<<B;
+	j = j - 1; // setting j one before the last string 
+	for (i = 0; i < j; i++, j--) 
+	{
+		temp = s[i]; // swapping 1st char with last everytime 
+		s[i] = s[j];
+		s[j] = temp;
+	}
+	cout << s;
+}
+
+int main()
+{
+	char* s;
+	int l;
+	cout << "Enter the Size of the Array " << endl;
+	cin >> l;
+	s = new char[l];
+	cout << "Enter the String " << endl;
+	cin >> s;
+
+	Reverse(s, l);
+	cout<<endl;
+	Reverse1(s);
+
+	return 0;
+
 }
