0831b13a510e96d98fcdde3b094437fcfc99276c
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2021-01-08 13:38:21 +0530

    Create 34 Add two numbers.cpp

---
+++ Array/Problems/34 Add two numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int a, b;
+		cin >> a >> b;
+		int c = a + b;
+		cout << c<<endl;;
+		continue;
+	}
+	return 0;
+}
