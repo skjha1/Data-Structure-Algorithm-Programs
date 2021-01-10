6da441206aeeb658ce003e58f30a212a49042c02
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2021-01-11 00:10:53 +0530

    Create string reverse.cpp

---
+++ Array/Geeksforgeeks problem/string reverse.cpp
@@ -0,0 +1,12 @@
+string reverseWord(string str){
+    
+    string rev = "";
+    for(int i = str.size()-1;i>=0 ;i--){
+        rev += str[i];
+    }
+    
+    return rev;
+    
+}
+
+===========================================================================
