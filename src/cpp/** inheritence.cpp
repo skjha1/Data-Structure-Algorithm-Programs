f4f4ca7f87a297b6725a66a2e5da76fdd908bebf
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2021-01-31 21:13:47 +0530

    Create ** inheritence.cpp

---
+++ src/cpp/** inheritence.cpp
@@ -0,0 +1,24 @@
+// Inheritance 
+// Aquaring property from already aquaired function
+// Suppose BMW , Toyota and other cars are aquired from the car class by some kind of modification and adding some more feathers 
+// if we made a class of rectangle class then we can make objects in this // and if we want to make one cuboid then we can borrow the feathers of the rect in cuboid 
+// we can reuse the code in another function 
+
+//class Base
+//{
+//public :
+//	int x;
+//	void Show()
+//	{
+//		cout << x;
+//	}
+//
+//};
+//class Derived : public Base
+//{
+//	public y;								Base <------------Derived class 
+//	void Display()
+//	{
+//		cout << x << y;
+//	}
+//};
