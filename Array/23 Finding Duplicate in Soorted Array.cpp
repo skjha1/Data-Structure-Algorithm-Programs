bab24e8186dde3c5fb07b15e17651149c48e51a5
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2021-03-21 10:12:53 +0530

    Array updated

--- Array/23 Finding Duplicate in Soorted Array.cpp
+++ Array/23 Finding Duplicate in Soorted Array.cpp
@@ -1,5 +1,55 @@
 #include<iostream>
 using namespace std;
+struct Array {
+	int* A;
+	int size;
+	int length;
+};
+
+void Find_Duplicate(struct Array* arr) {
+	int last_duplicate = 0;
+	for (int i = 0; i < arr->length; i++) {
+		if (arr->A[i] == arr->A[i + 1] && arr->A[i] != last_duplicate)
+		{
+			cout << arr->A[i]<<" ";
+			last_duplicate = arr->A[i];
+		}
+	}
+}
+int main() {
+	struct Array arr;
+	int no;
+	cout << "Enter the size of the array " << endl;
+	cin >> arr.size;
+	arr.A = new int[arr.size];
+	arr.length = 0;
+	cout << "Enter the size of the array" << endl;
+	cin >> no;
+	cout << "Enter the elements of the array " << endl;
+	for (int i = 0; i < no; i++) 
+		cin >> arr.A[i];
+	arr.length = no;
+	cout << "The duplicate elements are ! " << endl;
+	Find_Duplicate(&arr);
+	return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   -------------------------------------------------------------------------------------------------------------------------------------------
+#include<iostream>
+using namespace std;
 int main ()
 {
 	int A[10]={3,6,8,8,10,12,15,15,15,20};
