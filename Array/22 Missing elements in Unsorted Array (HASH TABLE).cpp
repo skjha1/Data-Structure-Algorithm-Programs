05d1b28e5fca3b5ad4de6bc6c418025581b79240
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2021-03-21 13:18:57 +0530

    Update 22 Missing elements in Unsorted Array (HASH TABLE).cpp

--- Array/22 Missing elements in Unsorted Array (HASH TABLE).cpp
+++ Array/22 Missing elements in Unsorted Array (HASH TABLE).cpp
@@ -1,10 +1,64 @@
+
+//Hash table with size equal to the largest element present in the array 
+
+#include<iostream>
+using namespace std;
+struct Array {
+	int* A;
+	int size;
+	int length;
+};
+void Missingelemet_Hash_table(struct Array* arr) {
+	int* H;
+	int i;
+	int low = Min(arr);
+	int high = Max(arr);
+	H = new int[high];
+	for (int i = 0; i < high; i++)H[i] = { 0 };
+	for (i = 0; i < arr->length-1; i++) {
+		H[arr->A[i]]++;
+	}
+	for (i = Min(arr); i <= high; i++) {
+		if (H[i] == 0)
+			cout << i<<" ";
+	}
+}
+int main() {
+	struct Array arr;
+	int no;
+	cout << "Enter the size of the array " << endl;
+	cin >> arr.size;
+	arr.A = new int[arr.size];
+	arr.length = 0;
+	cout << "Enter the size of the array" << endl;
+	cin >> no;
+	cout << "Enter the elements of the array " << endl;
+	for (int i = 0; i < no; i++) 
+		cin >> arr.A[i];
+	arr.length = no;
+	Missingelemet_Hash_table(&arr);
+return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------------
 #include<iostream>
 using namespace std;
 int main()
 {
 	int a[8] = { 1,2,3,5,6,8,12,14 };
 	int n = sizeof(a) / sizeof(int);
-	int h[14] = { 0 };					//Hash table with size equal to the largest element present in the array a ie 14
+	int h[14] = { 0 };					
 	for (int i = 0; i < n; i++)			//for loop to increment values at indices equal to the element in first array
 		h[a[i]]++;	
 	for (int i = 1; i <= 14; i++)
