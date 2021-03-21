352bd316f1d5eaee5ae8ae2a3d955d65fb9cc629
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2021-03-21 09:38:07 +0530

    Array updated

--- Array/21 Multiple Missing Elements in Sorted Array.cpp
+++ Array/21 Multiple Missing Elements in Sorted Array.cpp
@@ -5,23 +5,42 @@
 	//will increment diff till the difference a[i]-i not equal to diff to continue as soon as we find the missing element
 
 #include<iostream>
-using namespace std ;
-int main ()
-{
-	int A[11]= { 6,7,8,9,11,12,15,16,17,18,19 };
-	int diff=A[0];
-	int n= sizeof(A)/sizeof(int);
-	for (int i=0;i<n;i++)
-	{
-		if (A[i]-1!=diff)
-		{
-			while (diff<A[i]-i)
+using namespace std;
+struct Array {
+	int* A;
+	int size;
+	int length;
+};
+void MultipleMissingElement(struct Array* arr) {
+	int low = arr->A[0];
+	int difference = low - 0;
+	for (int i = 0; i < arr->length; i++) {
+		if (arr->A[i] - i != difference) {
+			while (difference < arr->A[i] - i)
 			{
-				cout<<"Missing Element is "<<i+diff<<endl;
-				diff++;
+				cout << i + difference<<" ";
+				difference++;
 			}
 		}
 	}
-	return 0;
+}
+
+int main() {
+	struct Array arr;
+	int no;
+	cout << "Enter the size of the array " << endl;
+	cin >> arr.size;
+	arr.A = new int[arr.size];
+	arr.length = 0;
+	cout << "Enter the size of the array" << endl;
+	cin >> no;
+	cout << "Enter the elements of the array " << endl;
+	for (int i = 0; i < no; i++) 
+		cin >> arr.A[i];
+	arr.length = no;
 	
+	cout << "The missing elements are ! " << endl;
+	MultipleMissingElement(&arr);
+	return 0;
+
 }
