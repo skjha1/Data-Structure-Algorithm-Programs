14dea47f0ec01217e1d28f74832ba756dcb95696
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2021-03-21 13:28:30 +0530

    array updated

--- Array/22 Missing elements in Unsorted Array (HASH TABLE).cpp
+++ Array/22 Missing elements in Unsorted Array (HASH TABLE).cpp
@@ -8,6 +8,22 @@ struct Array {
 	int size;
 	int length;
 };
+int Min(struct Array* arr) {
+	int min = INT_MAX;
+	for (int i = 0; i < arr->length; i++) {
+		if (arr->A[i] < min)
+			min = arr->A[i];
+	}
+	return min;
+}
+
+int Max(struct Array* arr) {
+	int max = INT_MIN;
+	for (int i = 0; i < arr->length; i++)
+		if (arr->A[i] > max)
+			max = arr->A[i];
+	return max;
+}
 void Missingelemet_Hash_table(struct Array* arr) {
 	int* H;
 	int i;
