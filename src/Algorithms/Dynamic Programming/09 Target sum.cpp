0aeb34b7cf22a68b9402cadbd86425cb511595c9
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2021-04-28 16:22:00 +0530

    Update 09 Target sum.cpp

--- src/Algorithms/Dynamic Programming/09 Target sum.cpp
+++ src/Algorithms/Dynamic Programming/09 Target sum.cpp
@@ -1,51 +1,49 @@
-#include <bits/stdc++.h>
-using namespace std;
 
-int CountSubsetsWithSum(int arr[], int n, int sum) {
+class Solution {
+public:
+   int CountSubsetsWithSum(vector<int>& nums,int sum) {
+       int n= nums.size();
 	int t[n + 1][sum + 1]; // DP - matrix
-	// initialization 
-  // here we are setting 1st row and 1st column 
-  // i denotes the size of the array 
-  // j denotes the target sum (subset sum)
+	// initialization
+	t[0][0] = 1;
+	int k = 1;
 	for (int i = 0; i <= n; i++) {
 		for (int j = 0; j <= sum; j++) {
-			if (i == 0) // when array(i) is empty than there is no meaning of sum of elements so return count of subset as 0;
+			if (i == 0 && j > 0)
 				t[i][j] = 0;
-			if (j == 0) // when sum(j) is zero and there is always a chance of empty subset so return count as 1;
-				t[i][j] = 1;
+			if (j == 0 && i > 0) {
+				if (nums[i - 1] == 0) {
+					t[i][j] = pow(2, k);
+					k++;
+				}
+				else
+					t[i][j] = t[i - 1][j];
+			}
 		}
 	}
 
 	for (int i = 1; i <= n; i++) {
 		for (int j = 1; j <= sum; j++) {
-			if (arr[i - 1] <= j) // when element in the list is less then target sum 
-				t[i][j] = t[i - 1][j - arr[i - 1]] + t[i - 1][j]; // either exclude or inxlude and add both of them to get final count 
+			if (nums[i - 1] <= j)
+				t[i][j] = t[i - 1][j - nums[i - 1]] + t[i - 1][j];
 			else
-				t[i][j] = t[i - 1][j]; // exclude when element in the list is greater then the sum 
+				t[i][j] = t[i - 1][j];
 		}
 	}
 
-	return t[n][sum]; // finally return the last row and last column element 
+	return t[n][sum];
 }
-
-int Targetsum(int arr[], int n, int diff) {
+    int findTargetSumWays(vector<int>& nums, int diff) {
+        int n= nums.size();
 	int sumOfArray = 0;
 	for (int i = 0; i < n; i++)
-		sumOfArray += arr[i]; // taking sum of the array 
+		sumOfArray += nums[i];
 
 	if ((sumOfArray + diff) % 2 != 0)
 		return 0;
 	else
-		return CountSubsetsWithSum(arr, n, (sumOfArray + diff) / 2);// we will get the number of array(subset) with particular sum 
-}
+		return CountSubsetsWithSum(nums, (sumOfArray + diff) / 2);
 
-signed main() {
-	int n; cin >> n;
-	int arr[n];
-	for (int i = 0; i < n; i++)
-		cin >> arr[i];
-	int diff; cin >> diff;
-
-	cout << Targetsum(arr, n, diff) << endl;
-	return 0;
-}
+    
+    }
+};
