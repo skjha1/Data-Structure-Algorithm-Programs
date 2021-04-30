0f1dfc8a5ccb6d14cc96ca325a036157294dab0d
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2021-04-30 11:51:35 +0530

    O(M*N)

---
+++ src/Algorithms/Dynamic Programming/20 Minimum insertion deletion to convert a to b.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int LCS(string X, string Y, int n, int m) {
+	int dp[n + 1][m + 1]; // DP - matrix
+
+	// base case of recursion --> for initialization of dp - matrix
+	for (int i = 0; i <= n; i++)
+		for (int j = 0; j <= m; j++)
+			if (i == 0 || j == 0)
+				dp[i][j] = 0;
+
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= m; j++)
+			if (X[i - 1] == Y[j - 1]) // when last character is same
+				dp[i][j] = 1 + dp[i - 1][j - 1];
+			else // when last character is not same -> pick max
+				dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
+
+	return dp[n][m];
+}
+
+void MinInsertDel(string X, string Y, int n, int m) {
+	int lcs_len = LCS(X, Y, n, m);
+	cout << "Minimum number of deletions = " << (n - lcs_len)<<endl;
+  cout << "Minimum number of insertions = " << (m - lcs_len)<<endl;
+}
+
+int main() {
+	string X, Y; cin >> X >> Y;
+	int n = X.length(), m = Y.length();
+
+	cout << MinInsertDel(X, Y, n, m) << endl;
+	return 0;
+}
