3fc7ed9d43b627a90e35103ca9fbe946fcb59678
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2021-05-11 20:30:42 +0530

    GFG question by aditya verma.

---
+++ Stack/07 Next Greater Element.cpp
@@ -0,0 +1,35 @@
+class
+{
+    public:
+  vector<long long> nextLargerElement(vector<long long> arr, int n){
+    vector<long long> v;
+    stack <long long> s;
+    for (int i=n-1;i>=0;i--){
+      if(s.size() ==0)
+        v.push_back(-1);
+      else if (s.size()>0 && s.top()>arr[i])
+      {
+        v.push_back(s.top());
+      }
+      else if (s.size()>0 && s.top()<=arr[i])
+      {
+        while(s.size()>0 && s.top()<=arr[i])
+        {
+          s.pop();
+        }
+        if (s.size()==0)
+        {
+          v.push_back(-1);
+        }
+        else
+        {
+          v.push_back(s.top());
+        }
+      }
+      s.push(arr[i]);
+    }
+    reverse(v.begin(),v.end());
+    return v;
+  }
+};
+      
