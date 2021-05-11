e3c4b5db2443ef96e6b9421ecc76c4bbc7781c4f
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2021-05-11 22:48:50 +0530

    stack aditya verma

---
+++ Stack/09 Nearest smaller to left.cpp
@@ -0,0 +1,37 @@
+class
+{
+    public:
+  vector<long long> nextLargerElement(vector<long long> arr, int n){
+    vector<long long> v; // creating a vector for storing result 
+    stack <long long> s; // creating a stack for temp. hold the values from array
+    for (int i=0;i<n;i++){ 
+      if(s.size() ==0) // when stack size is empty there is no element in stack return output as -1;
+        v.push_back(-1);
+      else if (s.size()>0 && s.top()<arr[i]) // when there is element in stack and stack top is smaller then array element 
+      {
+        v.push_back(s.top()); // take stack top in the result vector 
+      }
+      else if (s.size()>0 && s.top()>=arr[i]) // when there is element in stack and that element is greater then equal to array element 
+      {
+        while(s.size()>0 && s.top()>=arr[i]) // upto when there is element and stack top is greater then equal to array's element delete the element from stack
+        {
+          s.pop(); // delete the element from stack
+        }
+        if (s.size()==0) // when stack became empty return -1
+        {
+          v.push_back(-1);
+        }
+        else
+        {
+          v.push_back(s.top()); // else push stack top in the vector 
+        }
+      }
+      s.push(arr[i]); // push array in stack
+    }
+    return v;
+  }
+};
+     
+
+// Time Complexity: O(N)
+// Auxiliary Space: O(N)
