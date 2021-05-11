977540bf93b1cb37ddc6800e21a40dea185abfa1
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2021-05-11 20:41:29 +0530

    gfg question by Aditya verma

--- Stack/07 Next Greater Element.cpp
+++ Stack/07 Next Greater Element.cpp
@@ -2,34 +2,37 @@ class
 {
     public:
   vector<long long> nextLargerElement(vector<long long> arr, int n){
-    vector<long long> v;
-    stack <long long> s;
+    vector<long long> v; // creating a vector for storing result 
+    stack <long long> s; // creating a stack for temp. hold the values from array
     for (int i=n-1;i>=0;i--){
-      if(s.size() ==0)
+      if(s.size() ==0) // when stack size is empty there is no element in stack return output as -1;
         v.push_back(-1);
-      else if (s.size()>0 && s.top()>arr[i])
+      else if (s.size()>0 && s.top()>arr[i]) // when there is element in stack and stack top is greater then array element 
       {
-        v.push_back(s.top());
+        v.push_back(s.top()); // take stack top in the result vector 
       }
-      else if (s.size()>0 && s.top()<=arr[i])
+      else if (s.size()>0 && s.top()<=arr[i]) // when there is element in stack and that element is less then array element 
       {
-        while(s.size()>0 && s.top()<=arr[i])
+        while(s.size()>0 && s.top()<=arr[i]) // upto when there is element and stack top is less then array's element delete the element from stack
         {
-          s.pop();
+          s.pop(); // delete the element from stack
         }
-        if (s.size()==0)
+        if (s.size()==0) // when stack became empty return -1
         {
           v.push_back(-1);
         }
         else
         {
-          v.push_back(s.top());
+          v.push_back(s.top()); // else push stack top in the vector 
         }
       }
-      s.push(arr[i]);
+      s.push(arr[i]); // push array in stack
     }
-    reverse(v.begin(),v.end());
+    reverse(v.begin(),v.end()); // while returning reverse the vector and return it.
     return v;
   }
 };
-      
+     
+
+// Time Complexity: O(N)
+// Auxiliary Space: O(N)
