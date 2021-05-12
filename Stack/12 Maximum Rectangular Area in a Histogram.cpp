63a5b2a2c72c7c9830cba1998420f745eaac8f62
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2021-05-12 12:50:00 +0530

    Create 12 Maximum Rectangular Area in a Histogram.cpp

---
+++ Stack/12 Maximum Rectangular Area in a Histogram.cpp
@@ -0,0 +1,85 @@
+class Solution
+{
+  public:
+  long long getMaxArea(long long arr[], int n)
+    {
+    vector<int> left,right;
+    stack<pair<int,int>> s1,s2;
+    int pseudo_index =-1;
+    int pseudo_index1 =n;
+    for (int i=0;i<n;i++)
+    {
+      if (s1.size()==0)
+      {
+        left.push_back(pseudo_index);
+      }
+      else if (s1.size()>0 && s1.top().first<arr[i])
+      {
+        left.push_back(s1.top().second);
+      }
+      else if (s1.size()>0 && s1.top().first>=arr[i])
+      {
+         while(s1.size()>0 && s1.top().first>=arr[i])
+         {
+           s1.pop();
+         }
+        if (s1.size()==0)
+        {
+          left.push_back(pseudo_index);
+        }
+        else
+        {
+          left.push_back(s1.top().second);
+        }
+      }
+      s1.push({arr[i],i});
+    }
+    for (int i=n-1;i>=0;i--)
+    {
+      if (s2.size()==0)
+      {
+        right.push_back(pseudo_index1);
+      }
+      else if (s2.size()>0 && s2.top().first<arr[i])
+      {
+        right.push_back(s2.top().second);
+      }
+      else if (s2.size()>0 && s2.top().first >= arr[i])
+      {
+        while(s2.size()>0 && s2.top().first >= arr[i])
+        {
+          s2.pop();
+        }
+        if (s2.size()==0)
+        {
+          right.push_back(pseudo_index1);
+        }
+        else
+        {
+          right.push_back(s2.top().second);
+        }
+      }
+      s2.push({arr[i],i});
+    }
+    reverse(right.begin(),right.end());
+    long long m=INT_MIN;
+    for (long long i=0;i<n;i++)
+    {
+      m=max(m,(right[i]-left[i]-1)*arr[i]);
+    }
+    return m;
+  }
+};
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
